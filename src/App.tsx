14ff334170e0ff23a409b6c587e965b9c2a58ec0
shgusgh12 <https://github.com/shgusgh12>
2024-06-04 23:51:38 +0900

    feat : 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,31 @@ import { light, dark, ThemeName, getTheme } from './style/theme';
 import ThemeSwitcher from './components/common/header/ThemeSwitcher';
 import { useContext, useState } from 'react';
 import { BookStoreThemeProvider, ThemeContext } from './context/themeContext';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Error from './components/common/Error';
+import Signup from './pages/Signup'
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Layout><Home/></Layout>,
+    errorElement : <Error/>
+  },
+  {
+    path : '/books',
+    element : <Layout><div>도서목록</div></Layout>
+  },
+  {
+    path : '/signup',
+    element : <Layout><Signup /></Layout>,
+  }
+])
 
 function App() {
   return (
     <BookStoreThemeProvider>
-        <ThemeSwitcher />
-        <Layout>
-          <Home />
-        </Layout>
+      <ThemeSwitcher />
+      <RouterProvider router={router} />
     </BookStoreThemeProvider>
   );
 }
