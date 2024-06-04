3fe168e68626a4719f05f657b248a159e74dc79d
shgusgh12 <https://github.com/shgusgh12>
2024-06-04 23:52:05 +0900

    feat : 테마 생성

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,9 +1,38 @@
 export type ThemeName = "light" | "dark";
-type Colorkey = "primary" | "background" | "secondary" | "third";
-
+export type Colorkey = "primary" | "background" | "secondary" | "third" |
+'border' | 'text';
+export type HeadingSize = 'large' | 'medium' | 'small'; 
+export type ButtonSize = 'large' | 'medium' | 'small'; 
+export type ButtonScheme = 'primary' | 'normal';
+export type LayoutWidth = "large" | 'medium' | 'small';
 interface Theme{
     name : string;
     color : Record<Colorkey, string>;
+    heading : {
+        [key in HeadingSize] : {
+            fontSize : string;
+        }
+    }
+    button: {
+        [key in ButtonSize] : {
+            fontSize : string;
+            padding : string;
+        }
+    }
+    buttonScheme : {
+        [key in ButtonScheme] : {
+            color : string;
+            backgroundColor : string; 
+        }
+    };
+    borderRadius : {
+        default : string;
+    }
+    layout : {
+        width : {
+            [key in LayoutWidth] : string;
+        }
+    }
 }
 
 export const light: Theme = {
@@ -12,17 +41,67 @@ export const light: Theme = {
         primary : 'brown',
         background : 'lightgray',
         secondary : 'blue',
-        third : 'green'
+        third : 'green',
+        border : 'grey',
+        text : 'black'
+    },
+    heading : {
+        large : {
+            fontSize : '2rem'
+        },
+        medium : {
+            fontSize : '1.5rem'
+        },
+        small : {
+            fontSize : '1rem'
+        }
+    },
+    button : {
+        large : {
+            fontSize : '1.5rem',
+            padding : '1rem 2rem'
+        },
+        medium : {
+            fontSize : '1.0rem',
+            padding : '0.5rem 1rem'
+        },
+        small : {
+            fontSize : '0.75rem',
+            padding : '0.25rem 0.5rem',
+        },
+    },
+    buttonScheme : {
+        primary : {
+            color : 'white',
+            backgroundColor : 'midnightblue'
+        },
+        normal : {
+            color : 'black',
+            backgroundColor : 'lightgrey'
+        }
+    },
+    borderRadius: {
+        default : '4px',
+    },
+    layout : {
+        width : {
+            large : '1020px',
+            medium : '760px',
+            small : '320px'
+        }
     }
 };
 
 export const dark: Theme = {
+    ...light,
     name : 'dark',
     color : {
         primary : 'coral',
         background : 'midnightblue',
         secondary : 'blue',
-        third : 'green'
+        third : 'green',
+        border : 'grey',
+        text : 'black'
     }
 }
 
